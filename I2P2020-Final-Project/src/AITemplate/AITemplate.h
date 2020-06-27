281b1e85103ab16b59b695c90c203a3fbb783091
RongJheWu <rogerwu0111@gmail.com>
2020-06-27 22:06:29 +0800

    some AI update

--- I2P2020-Final-Project/src/AITemplate/AITemplate.h
+++ I2P2020-Final-Project/src/AITemplate/AITemplate.h
@@ -19,7 +19,14 @@ class AI : public AIInterface
 public:
     void init(bool order) override
     {
+        // playFirst initialize
         playFirst_count = 1;
+        int i, j;
+        for (i=0; i<3; ++i){
+            for (j=0; j<3; ++j){
+                playFirst_array[i][j] = false;
+            }
+        }
     }
 
     void callbackReportEnemy(int x, int y) override
